cc3d1565a538a16297c819d4328d62f954ce317e
Greg Bizup <g.bizup@gmail.com>
2022-09-12 16:44:05 -0700

    rotating proxies r and python

--- R/proxy_tools.R
+++ R/proxy_tools.R
@@ -31,33 +31,6 @@ proxy_GET <- function(url, proxy_ip, proxy_port, timeout = 3, ...) {
     return(page)
 }
 
-
-find_good_proxies <- function(proxies, test = "https://httpbin.org/get",
-                              https = TRUE, ...) {
-    if (https == TRUE) {
-        tryCatch({
-            proxies <- proxies[proxies["https"] == "yes", ]
-        }, error = function(cond) {
-            stop("proxy dataframe must have 'https' column")
-        })
-    }
-    good_proxies <- c()
-    for (p in seq_len(nrow(proxies))) {
-        ip <- proxies[p, ]$ip
-        port <- as.numeric(proxies[p, ]$port)
-        tryCatch({
-            message(sprintf("trying proxy %s https: %s",
-                            ip, proxies[p, ]$https))
-            proxy_GET(test, ip, port, ...)
-            message("proxy passed timeout test")
-            good_proxies <- append(good_proxies, p)
-        }, error = function(cond) {
-            message("proxy failed timeout test")
-        })
-    }
-    return(proxies[good_proxies, ])
-}
-
 rotate_proxies <- function(proxies, url, ...) {
     while (TRUE) {
         p <- sample(seq_len(nrow(proxies)), 1)
@@ -70,18 +43,10 @@ rotate_proxies <- function(proxies, url, ...) {
                 stop(sprintf("received %s \n", response["status_code"]))
             }
             message(sprintf("successfully retrieved %s", url))
-            print(response["status_code"])
             break
         }, error = function(cond) {
             message(sprintf("request failed with message %s: ", cond))
         })
     }
     return(response)
-}
-
-if (!interactive()) {
-    proxies <- get_spys_proxies()
-    filename <- sprintf("proxies/good_proxies_%s.csv", Sys.Date())
-    good <- find_good_proxies(proxies)
-    write.csv(good[2:5], filename)
 }
